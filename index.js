e577293e7faa79dd3997b4747b0f95b97bb4f439
Emanuel Kluge <mail@emanuel-kluge.de>
2021-01-16 16:15:17 +0100

    refactor: introduce explicit options object

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const github = require('@actions/github');
 const { run } = require('./lib/');
 
 const token = core.getInput('repo-token');
+const title = core.getInput('title');
+const basePath = core.getInput('base-path');
 const postsPerPage = core.getInput('posts-per-page');
 const { repo } = github.context;
 const octokit = github.getOctokit(token);
+const userOptions = {
+  ...(title ? { title } : undefined),
+  ...(basePath ? { basePath } : undefined),
+  ...(postsPerPage ? { postsPerPage } : undefined),
+};
 
-run({ octokit, repo, postsPerPage }).then(
+run({ octokit, repo, userOptions }).then(
   () => console.log('Successfully built Microblog'),
   (err) => {
     console.log(err.message);
