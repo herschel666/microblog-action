2f62862cd56938e535f4d7a7f08ab7e2db9242ca
Emanuel Kluge <mail@emanuel-kluge.de>
2021-01-18 20:18:01 +0100

    fix: respect custom posts page name in pathname

--- dist/index.js
+++ dist/index.js
@@ -26,7 +26,6 @@ const url = core.getInput('url', { required: true });
 const title = core.getInput('title');
 const description = core.getInput('description');
 const theme = core.getInput('theme');
-const maxWidth = core.getInput('max-width');
 const dateFormat = core.getInput('date-format');
 const postsPerPage = core.getInput('posts-per-page');
 const customStyles = core.getInput('custom-styles');
@@ -48,12 +47,11 @@ const userOptions = {
   url,
   lang,
   i18n,
-  maxWidth,
+  theme,
   dateFormat,
   postsPerPage,
   ...(title ? { title } : undefined),
   ...(description ? { description } : undefined),
-  ...(theme ? { theme } : undefined),
   ...(customStyles
     ? { customStyles: path.resolve(CWD, customStyles) }
     : undefined),
@@ -126,6 +124,7 @@ const {
   normalizeSiteUrl,
   copyStatic,
   loadCustomStyles,
+  createSlug,
 } = __nccwpck_require__(64024);
 
 exports.run = async ({ paths, octokit, repo, userOptions }) => {
@@ -134,12 +133,14 @@ exports.run = async ({ paths, octokit, repo, userOptions }) => {
     title: `${repo.owner}'s Microblog`,
   };
   const options = Object.assign(defaultOptions, userOptions);
-  const renderer = createRenderer(TEMPLATES);
-  const themeLink =
-    typeof options.theme === 'undefined' ? null : getThemeLink(options.theme);
+  const renderer = createRenderer(TEMPLATES, options.lang);
+  const themeLink = getThemeLink(options.theme);
   const postsPerPage = Number(options.postsPerPage);
   const { url, basePath } = normalizeSiteUrl(options.url);
-  const customStyles = await loadCustomStyles(options.customStyles);
+  const [baseStyles, customStyles] = await Promise.all([
+    fs.readFile(__nccwpck_require__.ab + "styles.css", 'utf8'),
+    loadCustomStyles(options.customStyles),
+  ]);
 
   await fs.rmdir(DIST, { recursive: true });
   await fs.mkdir(POSTS, { recursive: true });
@@ -159,7 +160,9 @@ exports.run = async ({ paths, octokit, repo, userOptions }) => {
     options.staticFrontpage &&
     pages.findIndex(({ filename }) => filename === options.staticFrontpage) >
       -1;
-  const postsIndexName = hasStaticFrontpage ? 'posts' : 'index';
+  const postsIndexName = hasStaticFrontpage
+    ? createSlug(options.i18n.posts, options.lang)
+    : 'index';
   const postContents = await Promise.all(
     posts.map((post) => renderMarkdown(post))
   );
@@ -171,12 +174,13 @@ exports.run = async ({ paths, octokit, repo, userOptions }) => {
     themeLink,
     url,
     basePath,
+    baseStyles,
     customStyles,
     hasStaticFrontpage,
+    theme: options.theme.replace(/\./g, '-'),
     lang: options.lang,
     title: options.title,
     description: options.description,
-    maxWidth: options.maxWidth,
     dateFormat: options.dateFormat,
     i18n: options.i18n,
     time: new Date(),
@@ -302,7 +306,9 @@ exports.renderMarkdown = async (item) => {
 const nunjucks = __nccwpck_require__(17006);
 const format = __nccwpck_require__(42168);
 
-exports.createRenderer = (folder) => {
+const { createSlug } = __nccwpck_require__(64024);
+
+exports.createRenderer = (folder, lang) => {
   const renderer = nunjucks.configure(folder);
 
   renderer.addFilter('date', (dateString, dateFormat) => {
@@ -313,6 +319,8 @@ exports.createRenderer = (folder) => {
     }
     return format(new Date(dateString), dateFormat);
   });
+
+  renderer.addFilter('slug', (str) => createSlug(str, lang));
 
   return renderer;
 };
@@ -326,6 +334,7 @@ exports.createRenderer = (folder) => {
 const path = __nccwpck_require__(85622);
 const { promises: fs } = __nccwpck_require__(35747);
 const cpy = __nccwpck_require__(14070);
+const slugify = __nccwpck_require__(99481);
 
 exports.getThemeLink = (theme) => {
   switch (theme) {
@@ -371,6 +380,13 @@ exports.loadCustomStyles = async (file) => {
     return null;
   }
 };
+
+exports.createSlug = (str, locale) =>
+  slugify(str, {
+    lower: true,
+    strict: true,
+    locale,
+  });
 
 
 /***/ }),
@@ -36879,7 +36895,7 @@ module.exports = __nccwpck_require__(28562).extend({
   implicit: [
     __nccwpck_require__(20721),
     __nccwpck_require__(64993),
-    __nccwpck_require__(11615),
+    __nccwpck_require__(43266),
     __nccwpck_require__(42705)
   ]
 });
@@ -37350,7 +37366,7 @@ module.exports = new Type('tag:yaml.org,2002:float', {
 
 /***/ }),
 
-/***/ 11615:
+/***/ 43266:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 "use strict";
@@ -71569,6 +71585,74 @@ module.exports = path => {
 
 /***/ }),
 
+/***/ 99481:
+/***/ (function(module) {
+
+
+;(function (name, root, factory) {
+  if (true) {
+    module.exports = factory()
+    module.exports.default = factory()
+  }
+  /* istanbul ignore next */
+  else {}
+}('slugify', this, function () {
+  var charMap = JSON.parse('{"$":"dollar","%":"percent","&":"and","<":"less",">":"greater","|":"or","¢":"cent","£":"pound","¤":"currency","¥":"yen","©":"(c)","ª":"a","®":"(r)","º":"o","À":"A","Á":"A","Â":"A","Ã":"A","Ä":"A","Å":"A","Æ":"AE","Ç":"C","È":"E","É":"E","Ê":"E","Ë":"E","Ì":"I","Í":"I","Î":"I","Ï":"I","Ð":"D","Ñ":"N","Ò":"O","Ó":"O","Ô":"O","Õ":"O","Ö":"O","Ø":"O","Ù":"U","Ú":"U","Û":"U","Ü":"U","Ý":"Y","Þ":"TH","ß":"ss","à":"a","á":"a","â":"a","ã":"a","ä":"a","å":"a","æ":"ae","ç":"c","è":"e","é":"e","ê":"e","ë":"e","ì":"i","í":"i","î":"i","ï":"i","ð":"d","ñ":"n","ò":"o","ó":"o","ô":"o","õ":"o","ö":"o","ø":"o","ù":"u","ú":"u","û":"u","ü":"u","ý":"y","þ":"th","ÿ":"y","Ā":"A","ā":"a","Ă":"A","ă":"a","Ą":"A","ą":"a","Ć":"C","ć":"c","Č":"C","č":"c","Ď":"D","ď":"d","Đ":"DJ","đ":"dj","Ē":"E","ē":"e","Ė":"E","ė":"e","Ę":"e","ę":"e","Ě":"E","ě":"e","Ğ":"G","ğ":"g","Ģ":"G","ģ":"g","Ĩ":"I","ĩ":"i","Ī":"i","ī":"i","Į":"I","į":"i","İ":"I","ı":"i","Ķ":"k","ķ":"k","Ļ":"L","ļ":"l","Ľ":"L","ľ":"l","Ł":"L","ł":"l","Ń":"N","ń":"n","Ņ":"N","ņ":"n","Ň":"N","ň":"n","Ō":"O","ō":"o","Ő":"O","ő":"o","Œ":"OE","œ":"oe","Ŕ":"R","ŕ":"r","Ř":"R","ř":"r","Ś":"S","ś":"s","Ş":"S","ş":"s","Š":"S","š":"s","Ţ":"T","ţ":"t","Ť":"T","ť":"t","Ũ":"U","ũ":"u","Ū":"u","ū":"u","Ů":"U","ů":"u","Ű":"U","ű":"u","Ų":"U","ų":"u","Ŵ":"W","ŵ":"w","Ŷ":"Y","ŷ":"y","Ÿ":"Y","Ź":"Z","ź":"z","Ż":"Z","ż":"z","Ž":"Z","ž":"z","Ə":"E","ƒ":"f","Ơ":"O","ơ":"o","Ư":"U","ư":"u","ǈ":"LJ","ǉ":"lj","ǋ":"NJ","ǌ":"nj","Ș":"S","ș":"s","Ț":"T","ț":"t","ə":"e","˚":"o","Ά":"A","Έ":"E","Ή":"H","Ί":"I","Ό":"O","Ύ":"Y","Ώ":"W","ΐ":"i","Α":"A","Β":"B","Γ":"G","Δ":"D","Ε":"E","Ζ":"Z","Η":"H","Θ":"8","Ι":"I","Κ":"K","Λ":"L","Μ":"M","Ν":"N","Ξ":"3","Ο":"O","Π":"P","Ρ":"R","Σ":"S","Τ":"T","Υ":"Y","Φ":"F","Χ":"X","Ψ":"PS","Ω":"W","Ϊ":"I","Ϋ":"Y","ά":"a","έ":"e","ή":"h","ί":"i","ΰ":"y","α":"a","β":"b","γ":"g","δ":"d","ε":"e","ζ":"z","η":"h","θ":"8","ι":"i","κ":"k","λ":"l","μ":"m","ν":"n","ξ":"3","ο":"o","π":"p","ρ":"r","ς":"s","σ":"s","τ":"t","υ":"y","φ":"f","χ":"x","ψ":"ps","ω":"w","ϊ":"i","ϋ":"y","ό":"o","ύ":"y","ώ":"w","Ё":"Yo","Ђ":"DJ","Є":"Ye","І":"I","Ї":"Yi","Ј":"J","Љ":"LJ","Њ":"NJ","Ћ":"C","Џ":"DZ","А":"A","Б":"B","В":"V","Г":"G","Д":"D","Е":"E","Ж":"Zh","З":"Z","И":"I","Й":"J","К":"K","Л":"L","М":"M","Н":"N","О":"O","П":"P","Р":"R","С":"S","Т":"T","У":"U","Ф":"F","Х":"H","Ц":"C","Ч":"Ch","Ш":"Sh","Щ":"Sh","Ъ":"U","Ы":"Y","Ь":"","Э":"E","Ю":"Yu","Я":"Ya","а":"a","б":"b","в":"v","г":"g","д":"d","е":"e","ж":"zh","з":"z","и":"i","й":"j","к":"k","л":"l","м":"m","н":"n","о":"o","п":"p","р":"r","с":"s","т":"t","у":"u","ф":"f","х":"h","ц":"c","ч":"ch","ш":"sh","щ":"sh","ъ":"u","ы":"y","ь":"","э":"e","ю":"yu","я":"ya","ё":"yo","ђ":"dj","є":"ye","і":"i","ї":"yi","ј":"j","љ":"lj","њ":"nj","ћ":"c","ѝ":"u","џ":"dz","Ґ":"G","ґ":"g","Ғ":"GH","ғ":"gh","Қ":"KH","қ":"kh","Ң":"NG","ң":"ng","Ү":"UE","ү":"ue","Ұ":"U","ұ":"u","Һ":"H","һ":"h","Ә":"AE","ә":"ae","Ө":"OE","ө":"oe","฿":"baht","ა":"a","ბ":"b","გ":"g","დ":"d","ე":"e","ვ":"v","ზ":"z","თ":"t","ი":"i","კ":"k","ლ":"l","მ":"m","ნ":"n","ო":"o","პ":"p","ჟ":"zh","რ":"r","ს":"s","ტ":"t","უ":"u","ფ":"f","ქ":"k","ღ":"gh","ყ":"q","შ":"sh","ჩ":"ch","ც":"ts","ძ":"dz","წ":"ts","ჭ":"ch","ხ":"kh","ჯ":"j","ჰ":"h","Ẁ":"W","ẁ":"w","Ẃ":"W","ẃ":"w","Ẅ":"W","ẅ":"w","ẞ":"SS","Ạ":"A","ạ":"a","Ả":"A","ả":"a","Ấ":"A","ấ":"a","Ầ":"A","ầ":"a","Ẩ":"A","ẩ":"a","Ẫ":"A","ẫ":"a","Ậ":"A","ậ":"a","Ắ":"A","ắ":"a","Ằ":"A","ằ":"a","Ẳ":"A","ẳ":"a","Ẵ":"A","ẵ":"a","Ặ":"A","ặ":"a","Ẹ":"E","ẹ":"e","Ẻ":"E","ẻ":"e","Ẽ":"E","ẽ":"e","Ế":"E","ế":"e","Ề":"E","ề":"e","Ể":"E","ể":"e","Ễ":"E","ễ":"e","Ệ":"E","ệ":"e","Ỉ":"I","ỉ":"i","Ị":"I","ị":"i","Ọ":"O","ọ":"o","Ỏ":"O","ỏ":"o","Ố":"O","ố":"o","Ồ":"O","ồ":"o","Ổ":"O","ổ":"o","Ỗ":"O","ỗ":"o","Ộ":"O","ộ":"o","Ớ":"O","ớ":"o","Ờ":"O","ờ":"o","Ở":"O","ở":"o","Ỡ":"O","ỡ":"o","Ợ":"O","ợ":"o","Ụ":"U","ụ":"u","Ủ":"U","ủ":"u","Ứ":"U","ứ":"u","Ừ":"U","ừ":"u","Ử":"U","ử":"u","Ữ":"U","ữ":"u","Ự":"U","ự":"u","Ỳ":"Y","ỳ":"y","Ỵ":"Y","ỵ":"y","Ỷ":"Y","ỷ":"y","Ỹ":"Y","ỹ":"y","‘":"\'","’":"\'","“":"\\\"","”":"\\\"","†":"+","•":"*","…":"...","₠":"ecu","₢":"cruzeiro","₣":"french franc","₤":"lira","₥":"mill","₦":"naira","₧":"peseta","₨":"rupee","₩":"won","₪":"new shequel","₫":"dong","€":"euro","₭":"kip","₮":"tugrik","₯":"drachma","₰":"penny","₱":"peso","₲":"guarani","₳":"austral","₴":"hryvnia","₵":"cedi","₸":"kazakhstani tenge","₹":"indian rupee","₺":"turkish lira","₽":"russian ruble","₿":"bitcoin","℠":"sm","™":"tm","∂":"d","∆":"delta","∑":"sum","∞":"infinity","♥":"love","元":"yuan","円":"yen","﷼":"rial"}')
+  var locales = JSON.parse('{"de":{"Ä":"AE","ä":"ae","Ö":"OE","ö":"oe","Ü":"UE","ü":"ue"},"vi":{"Đ":"D","đ":"d"}}')
+
+  function replace (string, options) {
+    if (typeof string !== 'string') {
+      throw new Error('slugify: string argument expected')
+    }
+
+    options = (typeof options === 'string')
+      ? {replacement: options}
+      : options || {}
+
+    var locale = locales[options.locale] || {}
+
+    var replacement = options.replacement === undefined ? '-' : options.replacement
+
+    var slug = string.split('')
+      // replace characters based on charMap
+      .reduce(function (result, ch) {
+        return result + (locale[ch] || charMap[ch] || ch)
+          // remove not allowed characters
+          .replace(options.remove || /[^\w\s$*_+~.()'"!\-:@]+/g, '')
+      }, '')
+      // trim leading/trailing spaces
+      .trim()
+      // convert spaces to replacement character
+      // also remove duplicates of the replacement character
+      .replace(new RegExp('[\\s' + replacement + ']+', 'g'), replacement)
+
+    if (options.lower) {
+      slug = slug.toLowerCase()
+    }
+
+    if (options.strict) {
+      // remove anything besides letters, numbers, and the replacement char
+      slug = slug
+        .replace(new RegExp('[^a-zA-Z0-9' + replacement + ']', 'g'), '')
+        // remove duplicates of the replacement character
+        .replace(new RegExp('[\\s' + replacement + ']+', 'g'), replacement)
+    }
+
+    return slug
+  }
+
+  replace.extend = function (customMap) {
+    for (var key in customMap) {
+      charMap[key] = customMap[key]
+    }
+  }
+
+  return replace
+}))
+
+
+/***/ }),
+
 /***/ 55244:
 /***/ ((module, exports, __nccwpck_require__) => {
 
@@ -79207,7 +79291,7 @@ function keepEscaping(opts, str, idx) {
 
 
 var copy = __nccwpck_require__(92448);
-var define = __nccwpck_require__(20663);
+var define = __nccwpck_require__(11615);
 var util = __nccwpck_require__(31669);
 
 /**
@@ -79291,7 +79375,7 @@ module.exports = extend;
 
 /***/ }),
 
-/***/ 20663:
+/***/ 11615:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 "use strict";
